7eab808de7a9d39a7ce6d85d93ea8eac5c3cc2b6
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-04-11 20:13:15 +0300

    connected my flask app to mySQL db, deleted zoo_service.sqlite

--- zoo_service.py
+++ zoo_service.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'zoo_service.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://myk_user:fwQH-totF8Pv@localhost:3306/lab6'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
