6b52a4b119354a4de340dc6ca4bb9e35c2d15742
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-04-11 20:15:06 +0300

    Slightly changed code

--- zoo_service.py
+++ zoo_service.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://myk_user:fwQH-totF8Pv@localhost:3306/lab6'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
