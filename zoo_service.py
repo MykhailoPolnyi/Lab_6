27764996bdfe2542e95ea9cb30f39372d16e4cb7
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-04-18 21:55:34 +0300

    Renamed variables names. Also, now if fish constructor failuers in POST method, server will send HTTP500

--- zoo_service.py
+++ zoo_service.py
@@ -3,21 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from marshmallow import fields, ValidationError
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://myk_user:fwQH-totF8Pv@localhost:3306/lab6'
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://myk_user:fwQH-totF8Pv@localhost:3306/lab6'
+DB = SQLAlchemy(APP)
+MA = Marshmallow(APP)
 
 
-class Fish(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    weight_in_kg = db.Column(db.Float)
-    thermoregulation = db.Column(db.String(15))
-    lifetime_years = db.Column(db.Float)
-    animal_type = db.Column(db.String(50))
-    required_aquarium_capacity_liters = db.Column(db.Float)
-    required_temperature = db.Column(db.Integer)
-    required_lighting_level = db.Column(db.String(20))
+class Fish(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    weight_in_kg = DB.Column(DB.Float)
+    thermoregulation = DB.Column(DB.String(15))
+    lifetime_years = DB.Column(DB.Float)
+    animal_type = DB.Column(DB.String(50))
+    required_aquarium_capacity_liters = DB.Column(DB.Float)
+    required_temperature = DB.Column(DB.Integer)
+    required_lighting_level = DB.Column(DB.String(20))
 
     def __init__(
             self, weight_in_kg, thermoregulation, lifetime_years, animal_type,
@@ -32,7 +32,7 @@ class Fish(db.Model):
         self.required_lighting_level = required_lighting_level
 
 
-class FishSchema(ma.Schema):
+class FishSchema(MA.Schema):
     id = fields.Int()
     weight_in_kg = fields.Float()
     thermoregulation = fields.String()
@@ -43,65 +43,65 @@ class FishSchema(ma.Schema):
     required_lighting_level = fields.String()
 
 
-fish_schema = FishSchema(exclude=["id"])
-fishes_schema = FishSchema(only=["id", "animal_type", "lifetime_years"], many=True)
+FISH_SCHEMA = FishSchema(exclude=["id"])
+FISH_LIST_SCHEMA = FishSchema(only=["id", "animal_type", "lifetime_years"], many=True)
 
 
-@app.route('/')
-def hello_page():
-    return 'Hello to the zoo aquariums!'
+@APP.route('/')
+def welcome_page():
+    return 'Welcome to the zoo aquariums!'
 
 
-@app.route('/fish', methods=['GET'])
+@APP.route('/fish', methods=['GET'])
 def get_all_fish():
-    all_fish = Fish.query.all()
-    return fishes_schema.jsonify(all_fish)
+    all_fish_list = Fish.query.all()
+    return FISH_LIST_SCHEMA.jsonify(all_fish_list)
 
 
-@app.route('/fish/<id>', methods=['GET'])
+@APP.route('/fish/<id>', methods=['GET'])
 def get_fish(id):
-    fish = Fish.query.get(id)
-    if not fish:
+    searched_fish = Fish.query.get(id)
+    if not searched_fish:
         return abort(404)
-    return fish_schema.jsonify(fish)
+    return FISH_SCHEMA.jsonify(searched_fish)
 
 
-@app.route('/fish', methods=['POST'])
+@APP.route('/fish', methods=['POST'])
 def add_fish():
     try:
-        params = fish_schema.load(request.json)
-        new_fish = Fish(**params)
-    except:
-        return abort(400)
-    db.session.add(new_fish)
-    db.session.commit()
-    return fish_schema.jsonify(new_fish), 201
-
-
-@app.route('/fish/<id>', methods=['PUT'])
-def update_fish(id):
-    fish = Fish.query.get(id)
-    if not fish:
-        return abort(404)
-    try:
-        new_params = fish_schema.load(request.json)
+        received_deserialized_values = FISH_SCHEMA.load(request.json)
     except ValidationError:
         return abort(400)
-    for param in new_params:
-        setattr(fish, param, request.json[param])
-    db.session.commit()
-    return fish_schema.jsonify(fish)
+    new_incoming_fish = Fish(**received_deserialized_values)
+    DB.session.add(new_incoming_fish)
+    DB.session.commit()
+    return FISH_SCHEMA.jsonify(new_incoming_fish), 201
 
 
-@app.route('/fish/<id>', methods=['DELETE'])
-def delete_fish(id):
-    fish = Fish.query.get(id)
-    if not fish:
+@APP.route('/fish/<id>', methods=['PUT'])
+def update_fish(id):
+    changed_fish = Fish.query.get(id)
+    if not changed_fish:
         return abort(404)
-    db.session.delete(fish)
-    db.session.commit()
-    return fish_schema.jsonify(fish), 204
+    try:
+        new_deserialized_values = FISH_SCHEMA.load(request.json)
+    except ValidationError:
+        return abort(400)
+    for fish_obj_updated_field, incoming_deserialized_value in new_deserialized_values.items():
+        setattr(changed_fish, fish_obj_updated_field, incoming_deserialized_value)
+    DB.session.commit()
+    return FISH_SCHEMA.jsonify(changed_fish)
+
+
+@APP.route('/fish/<id>', methods=['DELETE'])
+def delete_fish(id):
+    fish_to_delete = Fish.query.get(id)
+    if not fish_to_delete:
+        return abort(404)
+    DB.session.delete(fish_to_delete)
+    DB.session.commit()
+    return FISH_SCHEMA.jsonify(fish_to_delete), 204
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
